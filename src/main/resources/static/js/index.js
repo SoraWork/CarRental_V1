6d0be37257aa6a72f2f8528be99818ce48f7ca56
Hoai Phong <phongnguyenhoai2003@gmail.com>
2024-10-14 19:47:41 +0700

    Merge pull request #2 from SoraWork/feature_login_register_auth
    Feature login register auth

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById("show-menu");
     const header = document.querySelector('header');
-    const menu = document.querySelector('.mini-action-123'); // Thêm menu vào đây
+    const menu = document.querySelector('.mini-action-123');
+    const logoutPopup = document.getElementById('logout-popup');
+    const confirmLogout = document.getElementById('confirm-logout');
+    const cancelLogout = document.getElementById('cancel-logout');
+
     if (menuToggle && header && menu) {
-        menuToggle.addEventListener('click', function (event) {
-            // event.stopPropagation();
+        menuToggle.addEventListener('click', function () {
             menu.classList.toggle('show-menu');
             document.body.classList.toggle('overlay-active');
         });
@@ -16,5 +19,23 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
 
+        // Hiển thị popup khi click vào Logout
+        menu.addEventListener('click', function (event) {
+            if (event.target.closest('#logout')) {
+                event.preventDefault(); // Ngăn chặn hành động mặc định
+                logoutPopup.style.display = 'flex'; // Hiện popup
+            }
+        });
+
+        // Xử lý xác nhận logout
+        confirmLogout.addEventListener('click', function () {
+            logoutPopup.style.display = 'none'; // Ẩn popup
+            window.location.href = '/logout'; // Điều hướng đến URL logout
+        });
+
+        // Hủy bỏ logout
+        cancelLogout.addEventListener('click', function () {
+            logoutPopup.style.display = 'none'; // Ẩn popup
+        });
     }
 });
